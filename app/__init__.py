ff1002832c7ca96f632941ab8c81be0b443134d2
Raymond Polo <raymond.polo09@gmail.com>
2025-09-05 00:16:51 -0600

    Added Whitenoise

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,14 @@
-# app/__init__.py
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
 from config import Config
+from whitenoise import WhiteNoise
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
+login_manager.login_message = 'Please log in to access this page.'
 login_manager.login_message_category = 'info'
 mail = Mail()
 
@@ -21,34 +21,22 @@ def create_app(config_class=Config):
     login_manager.init_app(app)
     mail.init_app(app)
 
-    # Create upload folder if it doesn't exist
-    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+    # Add WhiteNoise middleware to serve static files in production
+    app.wsgi_app = WhiteNoise(app.wsgi_app, root='app/static/')
 
-    # Register Blueprints
-    from app.auth.routes import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint, url_prefix='/auth')
-
+    # Import and register blueprints
     from app.main.routes import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    # Import models to ensure they are known to SQLAlchemy
-    from app import models
-
-    # Context processor to inject notifications globally
-    @app.context_processor
-    def inject_notifications():
-        from flask_login import current_user
-        from app.models import Notification
-        if current_user.is_authenticated:
-            unread_notifications = Notification.query.filter_by(
-                user_id=current_user.id, is_read=False
-            ).order_by(Notification.timestamp.desc()).all()
-            return dict(unread_notifications=unread_notifications)
-        return dict(unread_notifications=[])
+    from app.auth.routes import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
     
-    # Create database and default user on first run
+    from app.main.context_processors import inject_notifications
+    app.context_processor(inject_notifications)
+
     with app.app_context():
         db.create_all()
-        models.User.create_default_superuser()
+        from app.models import User
+        User.create_default_superuser()
 
     return app
