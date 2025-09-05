83d8107585169db6df62377d4514d303e923d6ea
Raymond Polo <raymond.polo09@gmail.com>
2025-09-04 22:51:00 -0600

    Fixed issues with deployment failed

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, url_for, flash, request
-from flask_login import login_user, logout_user, login_required, current_user
-from itsdangerous import URLSafeTimedSerializer, SignatureExpired
-from app import db, mail
+from flask_login import login_user, logout_user, current_user
+from itsdangerous import URLSafeTimedSerializer
+from app import db, create_app
 from app.auth import auth
 from app.models import User
 from app.forms import LoginForm, RequestResetForm, ResetPasswordForm, SetPasswordForm
@@ -14,36 +14,35 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
-        if user and user.check_password(form.password.data):
-            if user.is_active:
-                login_user(user, remember=form.remember.data)
-                next_page = request.args.get('next')
-                return redirect(next_page) if next_page else redirect(url_for('main.index'))
-            else:
-                flash('Your account has not been activated yet. Please check your email for the activation link.', 'warning')
+        if user and user.check_password(form.password.data) and user.is_active:
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('main.index'))
         else:
-            flash('Login Unsuccessful. Please check email and password', 'danger')
-    # Corrected line below
+            flash('Login Unsuccessful. Please check email and password, or activate your account via the invitation link.', 'danger')
     return render_template('auth/login.html', title='Login', form=form)
 
 @auth.route('/logout')
-@login_required
 def logout():
     logout_user()
-    return redirect(url_for('main.index'))
+    return redirect(url_for('auth.login'))
 
 @auth.route('/set-password/<token>', methods=['GET', 'POST'])
 def set_password(token):
-    s = URLSafeTimedSerializer(db.get_app().config['SECRET_KEY'])
+    s = URLSafeTimedSerializer(create_app().config['SECRET_KEY'])
     try:
         email = s.loads(token, salt='account-setup-salt', max_age=86400) # 24 hours
-    except SignatureExpired:
-        flash('The activation link has expired.', 'danger')
+    except:
+        flash('The activation link is invalid or has expired.', 'danger')
         return redirect(url_for('auth.login'))
     
-    user = User.query.filter_by(email=email).first_or_404()
+    user = User.query.filter_by(email=email).first()
+    if not user:
+        flash('Invalid user.', 'danger')
+        return redirect(url_for('auth.login'))
+
     if user.is_active:
-        flash('Account already activated. Please log in.', 'info')
+        flash('This account has already been activated. Please log in.', 'info')
         return redirect(url_for('auth.login'))
 
     form = SetPasswordForm()
@@ -51,9 +50,9 @@ def set_password(token):
         user.set_password(form.password.data)
         user.is_active = True
         db.session.commit()
-        flash('Your account has been activated! You can now log in.', 'success')
+        flash('Your account has been activated! You are now able to log in.', 'success')
         return redirect(url_for('auth.login'))
-    return render_template('auth/set_password.html', title='Set Your Password', form=form)
+    return render_template('auth/set_password.html', title='Set Your Password', form=form, token=token)
 
 @auth.route('/reset_password', methods=['GET', 'POST'])
 def reset_request():
@@ -63,24 +62,23 @@ def reset_request():
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user:
-            s = URLSafeTimedSerializer(db.get_app().config['SECRET_KEY'])
+            s = URLSafeTimedSerializer(create_app().config['SECRET_KEY'])
             token = s.dumps(user.email, salt='password-reset-salt')
             email_body = f"""
-            <p>To reset your password, visit the following link:</p>
-            <p>This link is valid for 30 minutes.</p>
+            <p>To reset your password, please visit the following link. This link will expire in 1 hour.</p>
             """
             send_notification_email(
                 subject="Password Reset Request",
                 recipients=[user.email],
                 html_body=render_template(
                     'email/notification_email.html',
-                    title="Password Reset Request",
+                    title="Password Reset",
                     user=user,
                     body_content=email_body,
                     link=url_for('auth.reset_token', token=token, _external=True)
                 )
             )
-        flash('An email has been sent with instructions to reset your password.', 'info')
+        flash('If an account with that email exists, a password reset link has been sent.', 'info')
         return redirect(url_for('auth.login'))
     return render_template('auth/reset_request.html', title='Reset Password', form=form)
 
@@ -88,22 +86,23 @@ def reset_request():
 def reset_token(token):
     if current_user.is_authenticated:
         return redirect(url_for('main.index'))
-    s = URLSafeTimedSerializer(db.get_app().config['SECRET_KEY'])
+    s = URLSafeTimedSerializer(create_app().config['SECRET_KEY'])
     try:
-        email = s.loads(token, salt='password-reset-salt', max_age=1800) # 30 minutes
-    except SignatureExpired:
-        flash('The password reset link is invalid or has expired.', 'warning')
+        email = s.loads(token, salt='password-reset-salt', max_age=3600) # 1 hour
+    except:
+        flash('The password reset link is invalid or has expired.', 'danger')
         return redirect(url_for('auth.reset_request'))
     
     user = User.query.filter_by(email=email).first()
-    if user is None:
-        flash('Invalid or expired token.', 'warning')
-        return redirect(url_for('auth.reset_request'))
-        
+    if not user:
+        flash('Invalid user.', 'danger')
+        return redirect(url_for('auth.login'))
+
     form = ResetPasswordForm()
     if form.validate_on_submit():
         user.set_password(form.password.data)
         db.session.commit()
         flash('Your password has been updated! You are now able to log in.', 'success')
         return redirect(url_for('auth.login'))
-    return render_template('auth/reset_token.html', title='Reset Password', form=form)
+    return render_template('auth/reset_token.html', title='Reset Password', form=form, token=token)
+
