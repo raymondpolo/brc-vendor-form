4034607de7fe400d9ea2f2d0fa60e332e059df8c
Raymond Polo <raymond.polo09@gmail.com>
2025-10-08 13:58:03 -0600

    Timezone and Push notification fix

--- app/static/js/service-worker.js
+++ app/static/js/service-worker.js
@@ -1,11 +1,11 @@
 // app/static/js/service-worker.js
-self.addEventListener('push', function(event) {
+self.addEventListener('push', event => {
     const data = event.data.json();
     const options = {
         body: data.body,
-        icon: '/static/Logo.png',
+        icon: '/static/Logo.png', // Optional: icon to display
         data: {
-            url: data.url
+            url: data.url // URL to open when notification is clicked
         }
     };
     event.waitUntil(
@@ -13,8 +13,9 @@ self.addEventListener('push', function(event) {
     );
 });
 
-self.addEventListener('notificationclick', function(event) {
-    event.notification.close();
+self.addEventListener('notificationclick', event => {
+    event.notification.close(); // Close the notification
+    // Open the URL associated with the notification
     event.waitUntil(
         clients.openWindow(event.notification.data.url)
     );
