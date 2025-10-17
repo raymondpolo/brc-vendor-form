598199f6949d72d44f6bf53f53ca790772c285b5
Raymond Polo <raymond.polo09@gmail.com>
2025-10-17 17:11:48 -0600

    Added Push Notification

--- app/static/js/service-worker.js
+++ app/static/js/service-worker.js
@@ -1,12 +1,36 @@
 self.addEventListener('push', event => {
-    const data = event.data.json();
-    self.registration.showNotification(data.title, {
-        body: data.body,
-        icon: '/static/Logo.png',
-        data: {
-            url: data.link
-        }
-    });
+    let notificationData = {};
+
+    try {
+        // Try to parse the incoming data as JSON
+        const data = event.data.json();
+        notificationData = {
+            title: data.title,
+            body: data.body,
+            icon: '/static/Logo.png',
+            data: {
+                url: data.link
+            }
+        };
+    } catch (e) {
+        // If parsing fails, treat it as plain text (for testing)
+        notificationData = {
+            title: 'BRC Vendor Form',
+            body: event.data.text(),
+            icon: '/static/Logo.png',
+            data: {
+                url: '/'
+            }
+        };
+    }
+
+    event.waitUntil(
+        self.registration.showNotification(notificationData.title, {
+            body: notificationData.body,
+            icon: notificationData.icon,
+            data: notificationData.data
+        })
+    );
 });
 
 self.addEventListener('notificationclick', event => {
